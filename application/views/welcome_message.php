e52c87be20cdb81df0088a47b3dce483a8e37b8c
muhammadraisn <32321541+muhammadraisn@users.noreply.github.com>
2020-08-06 15:27:41 +0800

    upload

---
+++ application/views/welcome_message.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SMP & SMA ALMAZAYA ISLAMIC SCHOOL BANJARMASIN</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/fonts/fonts.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/select2.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/gray.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/form-elements.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/buttons.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+        <link rel="stylesheet" href="<?php echo base_url()?>assets/css/media-queries.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="<?php echo base_url()?>assets/images/logo.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url()?>assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url()?>assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url()?>assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="<?php echo base_url()?>assets/ico/apple-touch-icon-57-precomposed.png">
+
+    </head>
+
+    <body background="<?php echo base_url()?>assets/images/background.png">      
+        <!-- Services -->
+          <div class="container">
+            <div class="row">
+                <div class="col-sm-12 top-content-text">
+                  <h1>
+                    <span class="top-content-text-big">SMP & SMA ALMAZAYA ISLAMIC SCHOOL BANJARMASIN</span>
+                  </h1>
+                </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 services-box">
+                <a href="http://www.almazayaislamicschool.sch.id/">
+                <div class="services-box-icon"><img src="assets/img/icons/SMP.png" alt=""></div></a><h1>Junior high school</h1>             
+              </div>
+              <div class="col-sm-6 services-box">
+                <a href="http://www.almazayaislamicschool.sch.id/">
+                <div class="services-box-icon"><img src="assets/img/icons/SMA.png" alt=""></div></a><h1>Senior High School</h1>             
+              </div>
+            </div>
+
+        <!-- Portfolio -->
+        <div class="portfolio-container">
+          <div class="container">              
+              <div class="row">
+                  <div class="col-sm-3 services-box">
+                    <a href="http://virtualclass.almazayaislamicschool.sch.id/">
+                    <div class="services-box-icon"><img src="assets/img/icons/virtual-class.png" alt=""></div></a><h2>Vitual Class</h2>
+                  </div>
+                  <div class="col-sm-3 services-box">
+                    <a href="http://portal.almazayaislamicschool.sch.id/">
+                    <div class="services-box-icon"><img src="assets/img/icons/Portal.png" alt=""></div></a><h2>Portal</h2>
+                  </div>
+                  <div class="col-sm-3 services-box">
+                    <a href="http://www.almazayaislamicschool.sch.id/formulir-penerimaan-peserta-didik-baru">
+                    <div class="services-box-icon"><img src="assets/img/icons/PPDB.png" alt=""></div></a><h2>PPDB</h2>
+                  </div>
+                  <div class="col-sm-3 services-box">
+                    <a href="#">
+                    <div class="services-box-icon"><img src="assets/img/icons/library.png" alt=""></div></a><h2>Library</h2>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+          
+        <!-- Footer -->
+        <footer>
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-12 footer">
+                    <div class="footer-left">Copyright &copy; <?=date('Y');?> Al Mazaya Islamic School. All rights reserved.</div>
+                    
+                    <div class="footer-right">
+                      <a href="https://www.instagram.com/almazayaislamicschool/" class="fa fa-instagram"></a>
+                      <a href="https://www.youtube.com/channel/UCPWR9Ki4swRnEV_i2BHQPcQ" class="fa fa-youtube"></a>
+                    </div>
+                  </div>
+              </div>
+          </div>
+        </footer>
+        
+
+        <!-- Javascript -->
+        <script src="<?php echo base_url()?>assets/js/jquery-1.10.2.min.js"></script>
+        <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url()?>assets/js/jquery.backstretch.min.js"></script>
+        <script src="<?php echo base_url()?>assets/js/masonry.pkgd.min.js"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="<?php echo base_url()?>assets/js/jquery.ui.map.min.js"></script>
+        <script src="http://a.vimeocdn.com/js/froogaloop2.min.js"></script>
+        <script src="<?php echo base_url()?>assets/js/select2.min.js"></script>
+        <script src="<?php echo base_url()?>assets/js/jquery.gray.min.js"></script>
+        <script src="<?php echo base_url()?>assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="<?php echo base_url()?>assets/js/scripts.js"></script>
+        <script src="<?php echo base_url()?>assets/js/retina-1.1.0.min.js"></script>
+
+    <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
+    </body>
+
+</html>
+
