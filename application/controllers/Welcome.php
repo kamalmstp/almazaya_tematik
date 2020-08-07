e3d37913626d2f418acf2169845d7c9f12a3d095
muhammadraisn <m.rais.norhidayat@gmail.com>
2020-08-07 11:14:11 +0800

    tampilan

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,4 +22,23 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+
+	function data_schedule(){
+    $year = $this->db->get_where('settings' , array('type' => 'running_year'))->row()->description;
+    // $data = $this->db->get_where('class_routine', array('day' => date('l')))->result_array();
+        $this->db->select('c.name as cn, sc.name as scn, sb.name as sbn, t.name as tn, cr.time_start as crts, cr.time_start_min as crtsm, cr.time_end as crte, cr.time_end_min as crtem, o.status as stts');
+        $this->db->from('class_routine cr');
+        $this->db->join('class c', 'cr.class_id = c.class_id');
+        $this->db->join('section sc', 'cr.section_id = sc.section_id');
+        $this->db->join('subject sb', 'cr.subject_id = sb.subject_id');
+        $this->db->join('teacher t', 'sb.teacher_id = t.teacher_id');
+        $this->db->join('online o', 't.teacher_id = o.user_id', 'left');
+        $this->db->where('cr.day', date('l'));
+        $this->db->where('cr.year', $year);
+        $this->db->where('CAST(concat(HOUR(now()), ":", minute(now())) AS time) BETWEEN CAST(concat(time_start,":",time_start_min) AS time) and CAST(concat(time_end,":",time_end_min) AS time)');
+        $this->db->order_by('sc.section_id', 'asc');
+        $query = $this->db->get();
+        $data = $query->result_array();
+        echo json_encode($data);
+  }
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -34,6 +34,87 @@
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url()?>assets/ico/apple-touch-icon-72-precomposed.png">
         <link rel="apple-touch-icon-precomposed" href="<?php echo base_url()?>assets/ico/apple-touch-icon-57-precomposed.png">
 
+        <style>
+          h1,h2,p,a{
+            font-family: sans-serif;
+            font-weight: normal;
+          }
+
+          .jam-digital{
+            overflow: hidden;
+            width: 330px;
+            margin: 20px auto;
+            border: 5px solid #efefef;
+          }
+
+          .kotak{
+            float: left;
+            width: 110px;
+            height: 100px;
+            background-color: #189fff;
+          }
+
+        </style>
+        <style type="text/css">
+          .jam_analog_malasngoding {
+            background: #e7f2f7;
+            position: relative;
+            width: 120px;
+            height: 120px;
+            border: 8px solid #52b6f0;
+            border-radius: 50%;
+            padding: 10px;
+            margin:10px auto;
+          }
+
+          .xxx {
+            height: 100%;
+            width: 100%;
+            position: relative;
+          }
+
+          .jarum {
+            position: absolute;
+            width: 50%;
+            background: #232323;
+            top: 50%;
+            transform: rotate(90deg);
+            transform-origin: 100%;
+            transition: all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1);
+          }
+
+          .lingkaran_tengah {
+            width: 12px;
+            height: 12px;
+            background: #232323;
+            border: 2px solid #52b6f0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-left: -7px;
+            margin-top: -7px;
+            border-radius: 50%;
+          }
+
+          .jarum_detik {
+            height: 2px;
+            border-radius: 1px;
+            background: #F0C952;
+          }
+
+          .jarum_menit {
+            height: 2px;
+            border-radius: 2px;
+          }
+
+          .jarum_jam {
+            height: 4px;
+            border-radius: 2px;
+            width: 35%;
+            left: 15%;
+          }
+        </style>
+
     </head>
 
     <body background="<?php echo base_url()?>assets/images/background.png">      
@@ -56,9 +137,9 @@
                 <div class="services-box-icon"><img src="assets/img/icons/SMA.png" alt=""></div></a><h1>Senior High School</h1>             
               </div>
             </div>
+            <div class="blog-all-posts"><a class="button-2" href="#">REGISTER</a></div>
+          </div>
 
-        <!-- Portfolio -->
-        <div class="portfolio-container">
           <div class="container">              
               <div class="row">
                   <div class="col-sm-3 services-box">
@@ -79,8 +160,121 @@
                   </div>
               </div>
           </div>
+
+      <div class="portfolio-container">
+        <div class="container">
+              <div class="row">
+                <div class="team-box-photo">
+                  <div class="jam_analog_malasngoding">
+                    <div class="xxx">
+                      <div class="jarum jarum_detik"></div>
+                      <div class="jarum jarum_menit"></div>
+                      <div class="jarum jarum_jam"></div>
+                      <div class="lingkaran_tengah"></div>
+                    </div>
+                  </div>
+                  <h1 style="color: #FFFFFF;border:1px;text-align: -webkit-center;margin-bottom: 5px;font-weight: 300;margin-top: 10px;">
+                        <span id="jam"></span>
+                  </h1>
+                  <h4 style="color: #FFFFFF;text-align: -webkit-center;">
+                    <?=date('l, d M Y');?>
+                  </h4>
+                </div>
+                <div class="team-box-text">
+                  <h3 style="color: #FFFFFF;">JAM KE-</h3>
+                  <h1 style="color: #FFFFFF;text-align: -webkit-center;margin-bottom: 25px;font-weight: 300;margin-top: 10px;">
+                    <span id="ke"></span>
+                  </h1>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="panel panel-primary">
+                      <div class="panel-heading">
+                          <h3 class="panel-title">Scedule</h3>
+                      </div>
+                      <div class="panel-body">
+                    <div class="tabs-vertical-env">
+
+                      <div class="tab-content">
+
+                        <div class="tab-pane active">
+                          <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <td>Class</td>
+                                <td>Subject</td>
+                                <td>Time</td>
+                                <td>Teacher</td>
+                              </tr>
+                            </thead>
+                            <tbody id="target">
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>        
         </div>
       </div>
+
+      <div class="reviews-container">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-12 reviews">
+                      <h2>Customer reviews</h2>
+                      <div class="review-active"></div>
+                      <div class="review-single">
+                        <p>
+                          "Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
+                          quos dolores et quas molestias excepturi tempore"
+                          <br><br>
+                          <a class="review-author" href="">Steven Johnson, CEO</a>
+                        </p>
+                        <div class="review-single-pagination active"><span></span></div>
+                      </div>
+                      <div class="review-single">
+                        <p>
+                          "Neque porro quisquam est qui dolorem ipsum quia dolor sit ame consectetur adipisci velit sed quia non numquam 
+                          lorem dolor excepturi tempore"
+                          <br><br>
+                          <a class="review-author" href="">Jane Doe, Designer</a>
+                        </p>
+                        <div class="review-single-pagination"><span></span></div>
+                      </div>
+                      <div class="review-single">
+                        <p>
+                          "Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
+                          quos dolores et quas molestias excepturi tempore"
+                          <br><br>
+                          <a class="review-author" href="">Steven Johnson, CEO</a>
+                        </p>
+                        <div class="review-single-pagination"><span></span></div>
+                      </div>
+                      <div class="review-single">
+                        <p>
+                          "Neque porro quisquam est qui dolorem ipsum quia dolor sit ame consectetur adipisci velit sed quia non numquam 
+                          lorem dolor excepturi tempore"
+                          <br><br>
+                          <a class="review-author" href="">Jane Doe, Designer</a>
+                        </p>
+                        <div class="review-single-pagination"><span></span></div>
+                      </div>
+                      <div class="review-single">
+                        <p>
+                          "Accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
+                          quos dolores et quas molestias excepturi tempore"
+                          <br><br>
+                          <a class="review-author" href="">Steven Johnson, CEO</a>
+                        </p>
+                        <div class="review-single-pagination"><span></span></div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
           
         <!-- Footer -->
         <footer>
@@ -112,6 +306,159 @@
         <script src="<?php echo base_url()?>assets/js/jquery.magnific-popup.min.js"></script>
         <script src="<?php echo base_url()?>assets/js/scripts.js"></script>
         <script src="<?php echo base_url()?>assets/js/retina-1.1.0.min.js"></script>
+
+        <script>
+          window.setTimeout("waktu()", 1000);
+          window.setTimeout("agenda()", 1000);
+          window.setTimeout("www()", 1000);
+
+          function www(){
+            var time = Date.now();
+            var gmt = -(new Date()).getTimezoneOffset()/60;
+            var detik = Math.floor(time/1000);
+            seconds = ('0' + detik % 60).slice(-2);
+            var menit = Math.floor(detik/60);
+            minutes = ('0' + menit % 60).slice(-2);
+            var jam = Math.floor(menit/60);
+            hours = ('0' + (jam+gmt) % 24).slice(-2);
+            var view = hours + " : " + minutes + " : " + seconds;
+
+            document.getElementById("jam").innerHTML = view;
+            window.setTimeout("www()", 1000);
+          }
+
+          function waktu(){
+            var time = Date.now();
+            var gmt = -(new Date()).getTimezoneOffset()/60;
+            var detik = Math.floor(time/1000);
+            seconds = ('0' + detik % 60).slice(-2);
+            var menit = Math.floor(detik/60);
+            minutes = ('0' + menit % 60).slice(-2);
+            var jam = Math.floor(menit/60);
+            hours = ('0' + (jam+gmt) % 24).slice(-2);
+            var hh = hours + "" + minutes;
+
+            var isi = '';
+            setTimeout("waktu()", 1000);
+
+            if (hh >= 0715 && hh <= 0839) {
+              isi = 'Dhuha & Tahfidzh';
+            } else if(hh >= 0840 && hh <= 0919) {
+              isi = '1';
+            } else if(hh >= 0920 && hh <= 0959){
+              isi = '2';
+            } else if(hh >= 1000 && hh <= 1029){
+              isi = 'Break 1';
+            } else if(hh >= 1030 && hh <= 1109){
+              isi = '3';
+            } else if(hh >= 1110 && hh <= 1149){
+              isi = '4';
+            } else if(hh >= 1150 && hh <= 1239){
+              isi = 'Break 2 & Dzuhur';
+            } else if(hh >= 1240 && hh <= 1319){
+              isi = '5';
+            } else if(hh >= 1320 && hh <= 1359){
+              isi = '6';
+            } else if(hh >= 1400 && hh <= 1419){
+              isi = 'Break 3';
+            } else if(hh >= 1420 && hh <= 1459){
+              isi = '7';
+            } else if(hh >= 1500 && hh <= 1539){
+              isi = '8';
+            } else if(hh >= 1540 && hh <= 1559){
+              isi = 'Ashar';
+            } else {
+              isi = 'Get Home';
+            }
+            
+            document.getElementById("ke").innerHTML = isi;
+          }
+
+          function agenda(){
+            $.ajax({
+              type: 'POST',
+              url:'<?php echo base_url("home/data_agenda") ?>',
+              dataType:'json',
+              success: function(data){
+                var baris='';
+                for(var i=0;i<data.length;i++){
+                  baris += data[i].notice+"<br>";
+                }
+                $('#agenda').html(baris);
+              }
+            });
+          }
+
+        </script>
+        <script type="text/javascript">
+          const secondHand = document.querySelector('.jarum_detik');
+          const minuteHand = document.querySelector('.jarum_menit');
+          const jarum_jam = document.querySelector('.jarum_jam');
+
+          function setDate(){
+            const now = new Date();
+
+            const seconds = now.getSeconds();
+            const secondsDegrees = ((seconds / 60) * 360) + 90;
+            secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+            if (secondsDegrees === 90) {
+              secondHand.style.transition = 'none';
+            } else if (secondsDegrees >= 91) {
+              secondHand.style.transition = 'all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)'
+            }
+
+            const minutes = now.getMinutes();
+            const minutesDegrees = ((minutes / 60) * 360) + 90;
+            minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+            const hours = now.getHours();
+            const hoursDegrees = ((hours / 12) * 360) + 90;
+            jarum_jam.style.transform = `rotate(${hoursDegrees}deg)`;
+          }
+
+          setInterval(setDate, 1000)
+        </script>
+        <script>
+
+              window.setTimeout("ambilData()", 1000);
+
+                function ambilData(){
+                  var waktu = new Date();
+                  var hh = waktu.getHours();
+                  var mm = waktu.getMinutes();
+                  var ss = waktu.getSeconds();
+
+                  $.ajax({
+                    type: 'POST',
+                    url:'<?php echo base_url("Welcome/data_schedule") ?>',
+                    dataType:'json',
+                    success: function(data){
+                      // console.log(data);
+                      var baris='';
+                      for(var i=0;i<data.length;i++){
+                        if (data[i].stts == null) {
+                          baris += '<tr>'+
+                              '<td>'+data[i].cn+' ('+data[i].scn+') </td>'+
+                              '<td>'+data[i].sbn+'</td>'+
+                              '<td>'+data[i].crts+':'+data[i].crtsm+' - '+data[i].crte+':'+data[i].crtem+'</td>'+
+                              '<td>'+data[i].tn+' - '+'<div class="badge btn-success"><i>Online</i></div>'+'</td>'+
+                            '</tr>';
+                        } else {
+                          baris += '<tr>'+
+                              '<td>'+data[i].cn+' ('+data[i].scn+') </td>'+
+                              '<td>'+data[i].sbn+'</td>'+
+                              '<td>'+data[i].crts+':'+data[i].crtsm+' - '+data[i].crte+':'+data[i].crtem+'</td>'+
+                              '<td>'+data[i].tn+' - '+'<div class="badge btn-success">Online</div>'+'</td>'+
+                            '</tr>';
+                        }
+                      }
+                      $('#target').html(baris);
+                      }
+                    });
+
+                  window.setTimeout("ambilData()", 1000);
+                }
+              </script>
 
     <!--[if lt IE 10]>
             <script src="assets/js/placeholder.js"></script>
